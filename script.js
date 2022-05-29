0a5a2d876c0d8b48c7633abf7631782898392a99
Jaryodd Carter <jaryodd@gmail.com>
2022-05-29 17:28:58 -0600

    attached other classes

--- script.js
+++ script.js
@@ -6,29 +6,55 @@ var alpha2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', '
   var numbers = ['0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9'];
   var symbols = ['!' , '@' , '#' , '$' , '%' , '^' , '&' , '*' , '_' , '-' , '+' , '='];
 
+function characterRandom(array) {
+return array[Math.floor(Math.random() * array.legnth)]
+}
+
 function generatePassword() {
   var passwordLength = null;
   var specialCharacters = confirm("Do you want special characters?");
   var upperCase = confirm("Do you want upper case letters?");
   var lowerCase = confirm("Do you want lower case letters?");
   var numbers = confirm("Do you want numbers?");
-  console.log("specialCharacters", specialCharacters)
-  console.log("upperCase" , upperCase)
-  console.log("lowerCase", lowerCase)
-  console.log("numbers", numbers)
-  
+  // console.log("specialCharacters", specialCharacters)
+  // console.log("upperCase" , upperCase)
+  // console.log("lowerCase", lowerCase)
+  // console.log("numbers", numbers)
+
+  var userChoice = []
+  var userResults = []
+
   do {
   passwordLength = parseInt(prompt("Legnth of password?"));
   console.log(passwordLength);
   } while (passwordLength < 8 || passwordLength > 128 || Number.isNaN(passwordLength));
   
-  if (upperCase = true) {
-    for (let i = 0; i < passwordLength; i++) {
-      var randomNumber = Math.floor(Math.random() + upperCase.legnth) + "<br>";
-      password += upperCase.valueOf + (randomNumber + randomNumber +1)
-}
-console.log(upperCase)
+  if (specialCharacters === true) {
+    userChoice = userChoice.concat(symbols)
+    userResults.push(characterRandom(symbols))
+      console.log(userChoice)
+  console.log(userResults)
+  }
 
+  if (upperCase === true) {
+    userChoice = userChoice.concat(alpha2)
+    userResults.push(characterRandom(alpha2))
+      console.log(userChoice)
+  console.log(userResults)
+  }
+
+  if (lowerCase === true) {
+    userChoice = userChoice.concat(alpha1)
+    userResults.push(characterRandom(alpha1))
+      console.log(userChoice)
+  console.log(userResults)
+  }
+
+  if (numbers === true) {
+    userChoice = userChoice.concat(numbers)
+    userResults.push(characterRandom(numbers))
+      console.log(userChoice)
+  console.log(userResults)
   }
 
 } 
